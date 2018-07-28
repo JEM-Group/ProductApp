6d9068e80eb99ae6eb9b353d82f4403917b58082
sharebot-joe <josephikim2@gmail.com>
2018-07-28 14:12:36 -0700

    display youtube videos as embedded iframes

--- client/src/components/ResultList/ResultList.js
+++ client/src/components/ResultList/ResultList.js
@@ -1,18 +1,21 @@
 import React from "react";
+import './ResultList.css';
 
 const ResultList = props => (
   <ul className="list-group">
     {props.results.map(result => (
-      <li className="list-group-item" key={result.id}>
-        <a href={"https://www.youtube.com/watch?v=" + result.id.videoId}>
+      <li className="list-group-item" key={result.id.videoId}>
+        <h4>{result.snippet.title}</h4>
+        <iframe width="420" height="315"
+        src={"https://www.youtube.com/embed/" + result.id.videoId}>
+        </iframe>
+        {/*<a href={"https://www.youtube.com/watch?v=" + result.id.videoId}>
 	        <img
 	          alt={result.snippet.title}
 	          className="img-fluid"
 	          src = {result.snippet.thumbnails.high.url}
-	          // getting youtube videoID
-	          // src={result.id[videoId]}
 	        />
-	      </a>
+	      </a>*/}
       </li>
     ))}
   </ul>
