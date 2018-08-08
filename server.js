547d7a86b791409a90f8550c005f82a6f562bdf6
sharebot-joe <josephikim2@gmail.com>
2018-08-08 00:57:24 -0700

    skeleton files

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+var passport     = require('passport');
+var flash        = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// var port     = process.env.PORT || 8080;  from tutorial
 const PORT = process.env.PORT || 3001;
+
+// Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dashboardtestdata");
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,12 +28,26 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes')(app, passport); // load our routes and pass in our app and fully configured passport
+
 // Add routes, both API and view
 app.use(routes);
-
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dashboardtestdata");
 
 // Start the API server
 app.listen(PORT, function() {
