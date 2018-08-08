6280f22632660497987e4d4a0a618162cc335587
sharebot-joe <josephikim2@gmail.com>
2018-08-08 02:02:30 -0700

    local signup files

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'url' : "mongodb://localhost/dashboardtestdata" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    
+    // 'url' : 'mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");'
+
+};
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
+var express    = require("express");
+var bodyParser = require("body-parser");
+var mongoose   = require("mongoose");
+var routes     = require("./routes");
+var app        = express();
 var passport     = require('passport');
 var flash        = require('connect-flash');
 var morgan       = require('morgan');
@@ -11,22 +11,24 @@ var session      = require('express-session');
 
 var configDB = require('./config/database.js');
 
-// var port     = process.env.PORT || 8080;  from tutorial
-const PORT = process.env.PORT || 3001;
+var port = process.env.PORT || 8080; 
+// const PORT = process.env.PORT || 3001; // master branch
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dashboardtestdata");
+
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
+
+// Add routes, both API and view
+// app.use(routes);
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+  // app.use(express.static("client/build"));
 }
 
 
@@ -44,12 +46,11 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./routes')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-// Add routes, both API and view
-app.use(routes);
+
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(port, function() {
+  console.log(`🌎  ==> API Server now listening on port ${port}!`);
 });
